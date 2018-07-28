ed9c446f8a4060c358c8f41a498436907f06d855
Linming Ye <leaves.woods@hotmail.com>
2018-07-28 09:06:20 -0400

    vscode setting

---
+++ vscode_setting.js
@@ -0,0 +1,19 @@
+{
+  "window.zoomLevel": -2,
+  "editor.fontSize": 18,
+  "editor.tabSize": 2,
+  "editor.cursorStyle": "line",
+  "files.autoSave": "onFocusChange",
+  "git.autofetch": true,
+  "editor.minimap.enabled": false,
+  "terminal.integrated.fontSize": 16,
+  "files.exclude": {
+  "**/.git": true,
+  "**/.svn": true,
+  "**/.hg": true,
+  "**/CVS": true,
+  "**/.DS_Store": true,
+  "**/.vscode/": true,
+  "**/node_modules":true,
+},
+}
